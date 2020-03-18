72d6654eff4cf35f61c19cf7a42586c421c44688
marlinamukti <marlinamukti@gmail.com>
2020-03-18 21:19:11 +0700

    ganti https

--- src/components/App.js
+++ src/components/App.js
@@ -27,7 +27,7 @@ class App extends React.Component{
   }
 
   componentDidMount(){
-    fetch("http://www.mocky.io/v2/5c9105cb330000112b649af8")
+    fetch("https://www.mocky.io/v2/5c9105cb330000112b649af8")
     .then(data => data.json())
     .then(data => {
       // console.log(data.furniture_styles)
